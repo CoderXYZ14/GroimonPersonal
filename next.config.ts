046796c29b73dd28e24cd51a20ff2d2557a0f908
CoderXYZ14 <s1404islam@gmail.com>
2025-03-04 16:19:18 +0530

    build update 4

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {},
-  output: "standalone", // Optional: Useful if using serverless functions
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  /* config options here */
 };
+
+export default nextConfig;
