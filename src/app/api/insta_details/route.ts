26fa529f8811a6e85ab83377f3464d0035180d2b
CoderXYZ14 <s1404islam@gmail.com>
2025-04-05 20:02:39 +0530

    instagram token updated

--- src/app/api/insta_details/route.ts
+++ src/app/api/insta_details/route.ts
@@ -14,14 +14,14 @@ export async function POST(request: Request) {
 
     const response = await axios.get(`https://graph.instagram.com/v22.0/me`, {
       params: {
-        fields: "user_id,username",
+        fields: "id,username",
         access_token: accessToken,
       },
     });
 
-    const { user_id, username } = response.data;
+    const { id, username } = response.data;
 
-    return NextResponse.json({ user_id, username });
+    return NextResponse.json({ id, username });
   } catch (error) {
     console.error("Error fetching Instagram details:", error);
     return NextResponse.json(
