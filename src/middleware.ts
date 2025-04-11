c143a1faf85ba2da3db3244b6dd078507fc30534
CoderXYZ14 <s1404islam@gmail.com>
2025-04-11 13:20:01 +0530

    subdomains added

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,30 @@
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export async function middleware(request) {
+export async function middleware(request: NextRequest) {
   const url = request.nextUrl;
+  const hostname = request.headers.get("host") || "";
+  const isProduction = hostname === "groimon.com";
 
-  if (url.pathname.startsWith("/dashboard")) {
+  if (isProduction && url.pathname === "/redirect") {
+    return NextResponse.redirect(
+      new URL("https://redirect.groimon.com", request.url)
+    );
+  }
+
+  if (isProduction && url.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(
+      new URL(`https://app.groimon.com${url.pathname}`, request.url)
+    );
+  }
+
+  if (!isProduction && url.pathname.startsWith("/dashboard")) {
     const userDetails = request.cookies.get("user_details")?.value;
     if (!userDetails) {
       return NextResponse.redirect(new URL("/signin", request.url));
     }
   }
+
   return NextResponse.next();
 }
 
@@ -20,5 +36,6 @@ export const config = {
     "/dashboard/:path*",
     "/verify/:path*",
     "/your_insta_token",
+    "/redirectpage",
   ],
 };
