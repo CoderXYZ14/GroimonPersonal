f6d04cd938e2f9639afe2e402f0f8407c82186f2
CoderXYZ14 <s1404islam@gmail.com>
2025-04-17 17:29:24 +0530

    Subdomains added

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,20 +3,20 @@ import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
   const url = request.nextUrl;
-  // const hostname = request.headers.get("host") || "";
-  // const isProduction = hostname === "groimon.com";
+  const hostname = request.headers.get("host") || "";
+  const isProduction = hostname === "groimon.com";
 
-  // if (isProduction && url.pathname === "/redirect") {
-  //   return NextResponse.redirect(
-  //     new URL("https://redirect.groimon.com", request.url)
-  //   );
-  // }
+  if (isProduction && url.pathname === "/redirect") {
+    return NextResponse.redirect(
+      new URL("https://redirect.groimon.com", request.url)
+    );
+  }
 
-  // if (isProduction && url.pathname.startsWith("/dashboard")) {
-  //   return NextResponse.redirect(
-  //     new URL(`https://app.groimon.com${url.pathname}`, request.url)
-  //   );
-  // }
+  if (isProduction && url.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(
+      new URL(`https://app.groimon.com${url.pathname}`, request.url)
+    );
+  }
 
   if (url.pathname.startsWith("/dashboard")) {
     const userDetails = request.cookies.get("user_details")?.value;
