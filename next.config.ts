6f019ab89aaab2e0df0aa8948670a778faba1d3e
CoderXYZ14 <s1404islam@gmail.com>
2025-04-20 14:34:17 +0530

    image domains added in next config

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
       "lh3.googleusercontent.com", // For Google profile images
       "platform-lookaside.fbsbx.com", // For Facebook profile images (if needed)
       "instagram.fbom19-2.fna.fbcdn.net", // For Instagram profile images (if needed)
+      "scontent-iad3-1.cdninstagram.com", // Additional Instagram CDN domain
+      "scontent-iad3-2.cdninstagram.com", // Additional Instagram CDN domain
+      "*.cdninstagram.com", // Wildcard for all Instagram CDN subdomains
     ],
   },
 };
