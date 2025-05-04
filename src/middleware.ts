e68cbd1fe57d89824576ee3b309d5a7d3a204d50
CoderXYZ14 <s1404islam@gmail.com>
2025-05-04 05:44:06 +0530

    Middleware added

---
+++ src/middleware.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const url = request.nextUrl;
+  // const hostname = request.headers.get("host") || "";
+  // const isProduction = hostname === "groimon.com";
+
+  // if (isProduction && url.pathname === "/redirect") {
+  //   return NextResponse.redirect(
+  //     new URL("https://redirect.groimon.com", request.url)
+  //   );
+  // }
+
+  // if (isProduction && url.pathname.startsWith("/dashboard")) {
+  //   return NextResponse.redirect(
+  //     new URL(`https://app.groimon.com${url.pathname}`, request.url)
+  //   );
+  // }
+
+  if (url.pathname.startsWith("/dashboard")) {
+    const userDetails = request.cookies.get("user_details")?.value;
+    if (!userDetails) {
+      return NextResponse.redirect(new URL("/signin", request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/signin",
+    "/signup",
+    "/",
+    "/dashboard/:path*",
+    "/verify/:path*",
+    "/your_insta_token",
+    "/redirectpage",
+  ],
+};
